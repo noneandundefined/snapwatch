77de21bf2b57d07c823d97dffab0c086bb1ce5a9
Артём Власов <aworkprogramm@gmail.com>
2025-05-17 22:53:31 +0500

    added variable parts | added functions: ToneDataSet, Config | included 'System.Collections.Generic', 'System.IO', 'System.Windows.Documents' in ToneDataSet.cs

--- Engine/DataSet/ToneDataSet.cs
+++ Engine/DataSet/ToneDataSet.cs
@@ -1,10 +1,30 @@
-﻿namespace snapwatch.Engine.DataSet
+﻿using snapwatch.Core.Core;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Documents;
+
+namespace snapwatch.Engine.DataSet
 {
     public class ToneDataSet
     {
-        protected string[] energeticKeywords()
-        {
+        private readonly Config _config;
 
+        public ToneDataSet()
+        {
+            this._config = new Config();
+        }
+
+        protected string[] anticipationEmotionLexicon()
+        {
+            List<string> output = new List<string>();
+
+            foreach (var line in File.ReadLines(this._config.ReturnConfig().DATA_NRC_EMOTION_ANTICIPATION))
+            {
+                var parts = line.Split('\t');
+                output.Add(parts[0]);
+            }
+
+            return output.ToArray();
         }
 
         protected string[] joyfulKeywords()
