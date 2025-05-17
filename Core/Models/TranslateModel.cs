31d284b68a835f8a24fb8313c0cdfe1cb71ff4f3
Артём Власов <aworkprogramm@gmail.com>
2025-05-17 23:27:53 +0500

    added variable url | added functions: TranslateService, Config, RU_TO_EN | included 'System' in TranslateService.cs | included 'System.Text.Json.Serialization' in TranslateModel.cs

---
+++ Core/Models/TranslateModel.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace snapwatch.Core.Models
+{
+    public class TranslateModel
+    {
+        [JsonPropertyName("source-language")]
+        public string SourceLanguage { get; set; }
+
+        [JsonPropertyName("source-text")]
+        public string SourceText { get; set; }
+
+        [JsonPropertyName("destination-language")]
+        public string DestinationLanguage { get; set; }
+
+        [JsonPropertyName("destination-text")]
+        public string DestinationText { get; set; }
+
+        [JsonPropertyName("pronunciation")]
+        public Pronunciation Pronunciation { get; set; }
+
+        [JsonPropertyName("translations")]
+        public Translations Translations { get; set; }
+
+        [JsonPropertyName("definitions")]
+        public List<Definition> Definitions { get; set; }
+
+        [JsonPropertyName("see-also")]
+        public object SeeAlso { get; set; }
+    }
+
+    public class Pronunciation
+    {
+        [JsonPropertyName("source-text-phonetic")]
+        public string SourceTextPhonetic { get; set; }
+
+        [JsonPropertyName("source-text-audio")]
+        public string SourceTextAudio { get; set; }
+
+        [JsonPropertyName("destination-text-audio")]
+        public string DestinationTextAudio { get; set; }
+    }
+
+
+    public class Translations
+    {
+        [JsonPropertyName("all-translations")]
+        public List<List<object>> AllTranslations { get; set; }
+
+        [JsonPropertyName("possible-translations")]
+        public List<string> PossibleTranslations { get; set; }
+
+        [JsonPropertyName("possible-mistakes")]
+        public object PossibleMistakes { get; set; }
+    }
+
+    public class Definition
+    {
+        [JsonPropertyName("part-of-speech")]
+        public string PartOfSpeech { get; set; }
+
+        [JsonPropertyName("definition")]
+        public string Text { get; set; }
+
+        [JsonPropertyName("example")]
+        public string Example { get; set; }
+
+        [JsonPropertyName("other-examples")]
+        public object OtherExamples { get; set; }
+
+        [JsonPropertyName("synonyms")]
+        public object Synonyms { get; set; }
+    }
+}
--- Core/Service/TranslateService.cs
+++ Core/Service/TranslateService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using snapwatch.Core.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,18 @@ using System.Threading.Tasks;
 
 namespace snapwatch.Core.Service
 {
-    internal class TranslateService
+    public class TranslateService
     {
+        private readonly Config _config;
+
+        public TranslateService()
+        {
+            this._config = new Config();
+        }
+
+        public string RU_TO_EN(string text)
+        {
+            string url = $"{this._config.ReturnConfig().TRANSLATE_WWW_URL}?dl=en&text={text}";
+        }
     }
 }
