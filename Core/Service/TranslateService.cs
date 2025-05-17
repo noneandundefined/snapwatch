54c899c62cf0032bbe57314165ece882fa4c586a
Артём Власов <aworkprogramm@gmail.com>
2025-05-17 23:30:53 +0500

    added variables: response, stream, translated | changed value in variable url | added function HttpClient | included 'snapwatch.Core.Models', 'System.IO', 'System.Net.Http' in TranslateService.cs

--- Core/Service/TranslateService.cs
+++ Core/Service/TranslateService.cs
@@ -1,24 +1,37 @@
 ﻿using snapwatch.Core.Core;
+using snapwatch.Core.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Net.Http;
 
 namespace snapwatch.Core.Service
 {
     public class TranslateService
     {
+        private readonly HttpClient _http;
         private readonly Config _config;
 
         public TranslateService()
         {
+            this._http = new HttpClient();
             this._config = new Config();
         }
 
-        public string RU_TO_EN(string text)
+        public async string RU_TO_EN(string text)
         {
-            string url = $"{this._config.ReturnConfig().TRANSLATE_WWW_URL}?dl=en&text={text}";
+            string url = $"{this._config.ReturnConfig().TRANSLATE_WWW_URL}?dl=en&text={Uri.EscapeDataString(text)}";
+
+            HttpResponseMessage response = await this._http.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+
+            Stream stream = await response.Content.ReadAsStreamAsync();
+
+            var options = new System.Text.Json.JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            TranslateModel translated = await System.Text.Json.JsonSerializer.Deserialize<TranslateModel>(stream, options);
         }
     }
 }
