13b7bddec6618c140edcb9733740369d3be36a29
Артём Власов <aworkprogramm@gmail.com>
2025-05-19 12:26:10 +0500

    the 'App.xaml' file has been changed | the 'MainWindow.xaml' file has been changed | the 'snapwatch-win-setup-x86_x64.vdproj' file has been changed | the 'packages.config' file has been changed | the '.signature.p7s' file has been changed | the 'README.md' file has been changed | the 'VirtualizingWrapPanel.dll' file has been changed | the 'VirtualizingWrapPanel.dll' file has been changed

---
+++ snapwatch.UI/View/Home.xaml.cs
@@ -0,0 +1,48 @@
+﻿using snapwatch.UI.ViewModel;
+using System;
+using System.Windows.Controls;
+
+namespace snapwatch.UI.View
+{
+    /// <summary>
+    /// Логика взаимодействия для Home.xaml
+    /// </summary>
+    public partial class Home : UserControl
+    {
+        private bool _isLoading = false;
+
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void ScrollViewer_ScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            var scrollViewer = sender as ScrollViewer;
+            if (scrollViewer == null || this._isLoading)
+            {
+                return;
+            }
+
+            if (scrollViewer.ScrollableHeight == 0)
+            {
+                return;
+            }
+
+            if (scrollViewer.VerticalOffset >= scrollViewer.ScrollableHeight * 0.9)
+            {
+                this._isLoading = true;
+
+                if (DataContext is MovieCardVM vm)
+                {
+                    vm.LoadMoreMovies();
+                }
+
+                Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    this._isLoading = false;
+                }), System.Windows.Threading.DispatcherPriority.Background);
+            }
+        }
+    }
+}
---
+++ snapwatch.UI/ViewModel/PanelVM.cs
@@ -0,0 +1,48 @@
+﻿using snapwatch.Core.Service;
+using snapwatch.Core.Models;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace snapwatch.UI.ViewModel
+{
+    public class PanelVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Название меню в панеле
+        /// </summary>
+        public ObservableCollection<PanelItemsModel> MenuItems { get; set; }
+
+        /// <summary>
+        /// Команды
+        /// </summary>
+        private ICommand _menuItemClickCommand;
+        public ICommand MenuItemClickCommand
+        {
+            get
+            {
+                return _menuItemClickCommand ?? (_menuItemClickCommand = new RelayCommand(path =>
+                {
+                    Navigation _nav = new Navigation(Application.Current.MainWindow as MainWindow);
+                    _nav.SetFrameContent(path as string);
+                }));
+            }
+        }
+
+        public PanelVM()
+        {
+            MenuItems = new ObservableCollection<PanelItemsModel>
+            {
+                new PanelItemsModel { Image="pack://application:,,,/snapwatch.UI/Public/ico/layout-dashboard.png", PathView="pack://application:,,,/snapwatch.UI/View/Home.xaml", Title="Home" },
+                new PanelItemsModel { Image="pack://application:,,,/snapwatch.UI/Public/ico/text.png", PathView="pack://application:,,,/snapwatch.UI/View/Story.xaml", Title="Story" },
+            };
+        }
+    }
+}
