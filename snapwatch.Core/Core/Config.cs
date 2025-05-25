a0834aed128396fd2c27acbece17f533af01651c
Артём Власов <aworkprogramm@gmail.com>
2025-05-25 20:00:26 +0500

    added function database | the 'MovieCard.xaml' file has been changed | the 'MovieCardMini.xaml' file has been changed | added function MovieCardMini

--- snapwatch.Core/Core/Config.cs
+++ snapwatch.Core/Core/Config.cs
@@ -5,30 +5,57 @@ namespace snapwatch.Core.Core
 {
     public class AppConfig
     {
+        /// <summary>
+        /// путь к файлу json с фильмами
+        /// </summary>
         [Newtonsoft.Json.JsonProperty("MOVIES_JSON_READ")]
         public string MOVIES_JSON_READ { get; }
 
+        /// <summary>
+        /// путь к файлу .pidx к индексации страниц у фильмов
+        /// </summary>
         [Newtonsoft.Json.JsonProperty("MOVIES_PIDX_READ")]
         public string MOVIES_PIDX_READ { get; }
 
+        /// <summary>
+        /// путь к файлу .txt к JOY эмоциональной тональности
+        /// </summary>
         [Newtonsoft.Json.JsonProperty("DATA_NRC_EMOTION_JOY")]
         public string DATA_NRC_EMOTION_JOY { get; }
 
+        /// <summary>
+        /// путь к файлу .txt к SADNESS эмоциональной тональности
+        /// </summary>
         [Newtonsoft.Json.JsonProperty("DATA_NRC_EMOTION_SADNESS")]
         public string DATA_NRC_EMOTION_SADNESS { get; }
 
+        /// <summary>
+        /// путь к файлу .txt к TRUST эмоциональной тональности
+        /// </summary>
         [Newtonsoft.Json.JsonProperty("DATA_NRC_EMOTION_TRUST")]
         public string DATA_NRC_EMOTION_TRUST { get; }
 
+        /// <summary>
+        /// путь к файлу .txt к ANTICIPATION эмоциональной тональности
+        /// </summary>
         [Newtonsoft.Json.JsonProperty("DATA_NRC_EMOTION_ANTICIPATION")]
         public string DATA_NRC_EMOTION_ANTICIPATION { get; }
 
+        /// <summary>
+        /// путь к серверу для LSA/SVD алгоритмам
+        /// </summary>
         [Newtonsoft.Json.JsonProperty("SERVER_API_ADDRESS")]
         public string SERVER_API_ADDRESS { get; }
 
+        /// <summary>
+        /// путь к серверу для перевода текста
+        /// </summary>
         [Newtonsoft.Json.JsonProperty("TRANSLATE_WWW_URL")]
         public string TRANSLATE_WWW_URL { get; }
 
+        /// <summary>
+        /// API KEY для the movie database(tmdb)
+        /// </summary>
         [Newtonsoft.Json.JsonProperty("API_KEY_TMDB")]
         public string API_KEY_TMDB { get; }
 
---
+++ snapwatch.UI/UI/Components/MovieCardMini.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace snapwatch.snapwatch.UI.UI.Components
+{
+    /// <summary>
+    /// Логика взаимодействия для MovieCardMini.xaml
+    /// </summary>
+    public partial class MovieCardMini : UserControl
+    {
+        public MovieCardMini()
+        {
+            InitializeComponent();
+        }
+    }
+}
